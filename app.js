a1311bec408ac74f78e983a55ac8aecdab0e4143
Simone-Lauro-itis-pr <ssimone.lauro@itis.pr.it>
2025-04-09 10:20:55 +0200

    update

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
-
-app.use(bodyParser.urlencoded({ extended: true }))
 app.use((req, res, next)=>{
     console.log(req.method);
     console.log(req.path);
