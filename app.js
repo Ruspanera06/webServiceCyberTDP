8f4d76c9570a8814b700256eb32808ebe9790127
Ruspanera06 <harold.boado2006@gmail.com>
2025-04-09 10:17:30 +0200

    Initialized servetr

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use((req, res, next)=>{
+    console.log(req.method);
+    console.log(req.path);
+    next();
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  });
+const PORT = 3000
+app.listen(PORT, () => {
+    console.log(`✅ Server avviato su http://localhost:${PORT}`)
+})
